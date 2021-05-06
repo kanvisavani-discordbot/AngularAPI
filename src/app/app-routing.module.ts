1af291ac1dd19c804fa6af0d6718e57a0b032e99
Kanvi-discordbot <kanvisavani23@gmail.com>
2021-05-06 10:12:43 -0400

    observable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,11 +10,11 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'recipes', component: RecipesComponent, children: [
-    { path: '', component: RecipeStartComponent },
-    { path: 'new', component: RecipeEditComponent },
-    { path: ':id', component: RecipeDetailComponent },
-    { path: ':id/edit', component: RecipeEditComponent },
-  ] },
+      { path: '', component: RecipeStartComponent },
+      { path: 'new', component: RecipeEditComponent },
+      { path: ':id', component: RecipeDetailComponent },
+      { path: ':id/edit', component: RecipeEditComponent },
+    ] },
   { path: 'shopping-list', component: ShoppingListComponent },
 ];
 
